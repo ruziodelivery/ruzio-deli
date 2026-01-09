e1bae348c3b2aabb6821486cfdc233abbb471b4a
RuzioDelivery <ruzio.main@gmail.com>
2026-01-09 23:55:11 +0530

    first commit

--- ruzio/frontend/src/services/api.js
+++ ruzio/frontend/src/services/api.js
@@ -5,15 +5,15 @@
 
 import axios from 'axios';
 
-const API_URL = '/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
-// Create axios instance
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
 
 // Request interceptor - add auth token
 api.interceptors.request.use(
